613b8db78c187144710fdba24d77f25f80920fd7
Divaaayuu <divaayu2017.pkl@gmail.com>
2019-08-06 14:15:18 +0700

    first

---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1', function ($api){
+    $api->get('/me', function(){
+        $data = [
+            "name" => "Diva Ayu",
+            "nickname" => "Divaa",
+            "gender" => "female",
+            "class" => "XII RPL 2",
+        ];
+        return['status' => 200, 'data' => $data];
+    });
+    $api->post('/login', function(){
+        return ['status' => 204, 'data' => 'Success'];
+    });
+    $api->delete('/logout', function(){
+        return ['status' => 204, 'data' => 'Success'];
+    });
+    $api->post('/register', function(){
+        return ['status' => 204, 'data' => 'Success'];
+    });
+});
